b026485a86ecddc8b02c0409625e7692b76e59b0
Michael Hausenblas <michael.hausenblas@gmail.com>
2017-07-28 15:17:06 +0200

    fixes config map

--- promup.sh
+++ promup.sh
@@ -2,7 +2,7 @@
 
 oc new-project resorcerer
 oc apply -f deployments/all-cadvisor.yaml
-oc create configmap prom-config-cm --from-file=deployments/prometheus.yaml
+oc create configmap prom-config-cm --from-file=deployments/prometheus.yml
 oc apply -f deployments/all-prometheus.yaml
 oc expose service cadvisor
 oc expose service prometheus
